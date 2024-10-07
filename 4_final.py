b233b7aa26082cd18d6751e67f639ed33c399dab
Bell Eapen <github_public@gulfdoctor.net>
2024-10-07 03:04:55 +0000

    Refactor code to update imports and remove unnecessary dependencies

--- 4_final.py
+++ 4_final.py
@@ -1,15 +1,13 @@
 import pandas as pd
 from transformers import pipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-import time
-import torch
-from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from langchain_text_splitters import CharacterTextSplitter
 import tqdm
+import accelerate
 
 discharge_summaries = pd.read_csv('data/reduced_summaries.csv')
-subject_ids = discharge_summaries['subject_id'].unique()
 main_concepts = pd.read_csv('data/main_concepts.csv')
+subject_ids = discharge_summaries['subject_id'].unique()
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
 model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct",
                                              device_map="auto",
@@ -19,15 +17,6 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct
 generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 prompt_templates = [
-    # Identify the main concept in the text
-    (
-        "You are a clinician reviewing a patient's discharge summary. "
-        "Identify two main concepts in the text separated by ,.\n"
-        "Text: {prompt}\n"
-        "Main concepts::"
-    ),
-    # dialogue style template with a system prompt
-    # dialogue style template with a system prompt
     (
         "You are a clinician summarizing a patient's discharge summary. "
         "Do not include your tasks or instructions.\n"
@@ -36,15 +25,29 @@ prompt_templates = [
         "Text: {prompt}\n"
         "Summary::"
     ),
+    (
+        "You are a clinician summarizing a patient's discharge summary. "
+        "Do not include your tasks or instructions.\n"
+        "Do not include name, date, or other identifying information.\n"
+        "Summarize the following text in one paragraph for the main theme: {concept}\n"
+        "Mention if any of the following are present: {expanded_concepts}\n"
+        "Text: {prompt}\n"
+        "Summary::"
+    ),
 ]
 
 # https://huggingface.co/docs/transformers/pipeline_tutorial
-def stream_data(subject_id):
+def stream_data(subject_id, idx=1):
     docs = []
     for index, notes in discharge_summaries[discharge_summaries['subject_id'] == subject_id].iterrows():
         discharge_note = notes['text']
-        concept = main_concepts[main_concepts['subject_id'] == subject_id]['concept'].values[0]
-        docs.append(prompt_templates[1].format(concept=concept, prompt=discharge_note))
+        concept = discharge_summaries[discharge_summaries['subject_id'] == subject_id]['concept'].values[0]
+        expanded_concepts = str(main_concepts[main_concepts['subject_id'] == subject_id]['expanded_concepts'].values[0])
+        print(f"Subject ID: {subject_id}, Concept: {concept}, Expanded Concepts: {expanded_concepts}")
+        if idx == 0:
+            docs.append(prompt_templates[0].format(concept=concept, prompt=discharge_note))
+        else:
+            docs.append(prompt_templates[1].format(concept=concept, prompt=discharge_note, expanded_concepts=expanded_concepts))
     for doc in docs:
         yield doc
 
@@ -55,15 +58,16 @@ excluded = [10000826, 10000032, 10000980, 10001186]
 for subject_id in tqdm.tqdm(subject_ids):
     if subject_id in excluded:
         continue
-    concept = main_concepts[main_concepts['subject_id'] == subject_id]['concept'].values[0]
-    summary = ""
-    for response in generator(stream_data(subject_id), max_new_tokens=256):
-        full_response = response[0]["generated_text"].split("::")[-1]
-        summary += full_response.split("##")[0].strip() + "\n" # remove the system prompt that gets appended
-        print(full_response.split("##")[0].strip(), "\n")
-        print(f"Subject ID: {subject_id}, Concept: {concept}, Length: {len(summary)}")
-        print("---------------------------------------------------------------")
+    concept = discharge_summaries[discharge_summaries['subject_id'] == subject_id]['concept'].values[0]
+    summary = ["", ""]
+    for idx in range(2):
+        for response in generator(stream_data(subject_id, idx), max_new_tokens=128):
+            full_response = response[0]["generated_text"].split("::")[-1]
+            summary[idx] += full_response.split("##")[0].strip() + "\n" # remove the system prompt that gets appended
+            print(full_response.split("##")[0].strip(), "\n")
+            print(f"Subject ID: {subject_id}, Concept: {concept}, Length: {len(summary[idx])}, Index: {idx}")
+            print("---------------------------------------------------------------")
 
-    summaries.append([subject_id, summary, concept])
-df = pd.DataFrame(summaries, columns=['subject_id', 'text', 'concept'])
+    summaries.append([subject_id, summary[0], summary[1], concept])
+df = pd.DataFrame(summaries, columns=['subject_id', 'text', 'jog_memory', 'concept'])
 df.to_csv('data/final_summaries.csv', index=False)
