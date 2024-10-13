28823953fe49693a61668527ae60d1e560611bef
Bell Eapen <github_public@gulfdoctor.net>
2024-10-13 14:06:48 +0000

    Refactor code to update imports, remove unnecessary dependencies, and adjust discharge sample size selection

--- 1_find_concepts.py
+++ 1_find_concepts.py
@@ -3,6 +3,8 @@ import pandas as pd
 from gensim.models import Word2Vec
 from huggingface_hub import snapshot_download
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import re
+from tqdm import tqdm
 
 df = pd.read_csv('~/data/discharge_5000.csv')
 sample = df.sample(n=500) # 30
@@ -12,7 +14,7 @@ sample = df.sample(n=500) # 30
 
 subject_id = sample['subject_id'].unique()
 
-subject_id = subject_id[0:20]
+subject_id = subject_id[0:25]
 print(f"Subject IDs: {subject_id}")
 discharge_summaries = df[df['subject_id'].isin(subject_id)]
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
@@ -69,13 +71,14 @@ prompt_templates = [
 subject_id = 0
 concept = ""
 main_concepts = []
-excluded = [10000826, 10000032, 10000980, 10001186, 10038849]
+# excluded = [10000826, 10000032, 10000980, 10001186, 10038849]
 print("Identifying main concepts in discharge summaries...")
-for index, row in discharge_summaries.iterrows():
+# for index, row in discharge_summaries.iterrows():
+for index, row in tqdm(discharge_summaries.iterrows(), total=discharge_summaries.shape[0]):
         if subject_id != row['subject_id']:
             subject_id = row['subject_id']
-            if subject_id in excluded:
-                continue
+            # if subject_id in excluded:
+            #     continue
             concept = ""
             discharge_note = row['text'][:2500]
             response = generator(
@@ -83,8 +86,9 @@ for index, row in discharge_summaries.iterrows():
             )
             concept = response[0]["generated_text"].split("::")[-1].strip().split("\n")[0]
             expanded_concepts = expand_concept(concept.lower().strip().replace(" ", "_"))
+            expanded_concepts = re.sub(r'\W+', ' ', str(expanded_concepts)).strip()
             print(f"Subject ID: {subject_id}, Main Concept: {concept}, Expanded Concepts: {expanded_concepts}")
-            if expanded_concepts:
+            if len(expanded_concepts) > 5:
                 main_concepts.append([subject_id, concept, expanded_concepts, row["text"]])
 df = pd.DataFrame(main_concepts, columns=['subject_id', 'concept', 'expanded_concepts', 'text'])
 df.to_csv('~/data/main_concepts.csv', index=False)
