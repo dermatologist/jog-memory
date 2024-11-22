bd2593fe48ff3a82cdbc899d7e13a475665b6095
Bell Eapen <github_public@gulfdoctor.net>
2024-11-22 19:23:55 +0000

    Refactor subject ID selection and processing in MIMIC-IV script; enhance text handling and improve RAG integration

--- mimic.py
+++ mimic.py
@@ -13,58 +13,57 @@ max_tokens = 256
 
 df = pd.read_csv('~/data/discharge_5000.csv')
 sample = df.sample(n=500) # 30
-subject_id = sample['subject_id'].unique()
-subject_id = subject_id[0:30]
+
+unique_values = sample['subject_id'].value_counts()
+subject_id = unique_values[unique_values == 2].index # <3
+subject_id.append(unique_values[unique_values == 3].index)
+subject_id = subject_id.unique()
+
+# subject_id = sample['subject_id'].unique()
+subject_ids = subject_id[0:30]
 print(f"Subject IDs: {subject_id}")
-discharge_summaries = df[df['subject_id'].isin(subject_id)]
+# discharge_summaries = df[df['subject_id'].isin(subject_id)]
 
 jog_memory = JogMemory(
     n_ctx=n_ctx,
     max_tokens=max_tokens,
 )
-jog_rag = JogRag()
+jog_rag = JogRag(
+    n_ctx=n_ctx,
+)
 
-subject_id = 0
-for index, row in tqdm(discharge_summaries.iterrows(), total=discharge_summaries.shape[0]):
-    # alphaneumeric = re.sub(r'\W+', ' ', row['text']).strip()
-    alphaneumeric = row['text']
-    if subject_id != row['subject_id'] and subject_id == 0:
+# for seach subject_id
+for subject_id in tqdm(subject_ids):
+    discharge_summaries = df[df['subject_id'] == subject_id]
+    jog_memory.clear_text()
+    for index, row in tqdm(discharge_summaries.iterrows(), total=discharge_summaries.shape[0]):
+        alphaneumeric = re.sub(r'\W+', ' ', row['text']).strip()
         jog_memory.append_text(alphaneumeric)
-        subject_id = row['subject_id']
+    # Main prodessing
+    # print(jog_memory.get_text())
+    print(f"Length of text: {len(jog_memory.get_text())}")
+    print(index, subject_id)
+
+    # identify main concept and expanded concepts
+    concept = jog_memory.find_concept()
+    expanded_concepts = jog_memory.expand_concept(concept)
+
+    # continue if concept is not found or expanded concepts list is empty
+    if concept == "" or len(expanded_concepts) == 0:
+        continue
+
+    # RAG if length of text exceeds context window size
+    if len(jog_memory.get_text()) > (n_ctx-300):
+        docs = jog_rag.split_text(jog_memory.get_text(), subject_id, concept, expanded_concepts)
+        context = jog_rag.get_context(concept, expanded_concepts)
     else:
-        # Main prodessing
-        # print(jog_memory.get_text())
-        print(f"Length of text: {len(jog_memory.get_text())}")
-        print(index, subject_id)
+        context = jog_memory.get_text()
 
-        # identify main concept and expanded concepts
-        concept = jog_memory.find_concept()
-        expanded_concepts = jog_memory.expand_concept(concept)
-
-        # continue if concept is not found or expanded concepts list is empty
-        if concept == "" or len(expanded_concepts) == 0:
-            jog_memory.clear_text()
-            jog_memory.append_text(alphaneumeric)
-            subject_id = row['subject_id']
-            continue
-
-        # RAG if length of text exceeds context window size
-        if len(jog_memory.get_text()) > (n_ctx-300):
-            docs = jog_rag.split_text(jog_memory.get_text(), subject_id, concept, expanded_concepts)
-            context = jog_rag.get_context(concept, expanded_concepts)
-        else:
-            context = jog_memory.get_text()
-
-        # Summarize the context
-        print(f"Subject ID: {subject_id}, Main Concept: {concept}, Expanded Concepts: {expanded_concepts}")
-        print("Traditional Summary: \n")
-        print(jog_memory.summarize(concept))
-        print("---------------------------------------------------------------\n\n")
-        print("Expanded Summary: \n")
-        print(jog_memory.summarize(concept, expanded_concepts))
-        print("---------------------------------------------------------------\n\n")
-
-        # Clear the text
-        jog_memory.clear_text()
-        jog_memory.append_text(row['text'])
-        subject_id = row['subject_id']
+    # Summarize the context
+    print(f"Subject ID: {subject_id}, Main Concept: {concept}, Expanded Concepts: {expanded_concepts}")
+    print("Traditional Summary: \n")
+    print(jog_memory.summarize(concept))
+    print("---------------------------------------------------------------\n\n")
+    print("Expanded Summary: \n")
+    print(jog_memory.summarize(concept, expanded_concepts))
+    print("---------------------------------------------------------------\n\n")
--- src/jog_memory/rag.py
+++ src/jog_memory/rag.py
@@ -1,6 +1,6 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 import os
 import shutil
@@ -10,10 +10,13 @@ logging.getLogger("langchain_text_splitters.base").setLevel(logging.ERROR)
 
 class JogRag:
 
-    def __init__(self):
+    def __init__(self, n_ctx=None
+                 ):
         self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         self.CHROMA_PATH = "/tmp/chroma"
         self.db = None
+        self.n_ctx = n_ctx
+
 
     def save_to_chroma(self, docs, subject_id):
         if os.path.exists(self.CHROMA_PATH):
@@ -28,13 +31,17 @@ class JogRag:
         self.db = db
         return db
 
-    def split_text(self, text, subject_id, concept="", expanded_concepts=[], chunk_size=64, chunk_overlap=5, save_to_chroma=True):
+    def split_text(self, text, subject_id, concept="", expanded_concepts=[], chunk_size=64, chunk_overlap=5, k=5, save_to_chroma=True):
         docs = []
-        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        if self.n_ctx:
+            # chunk_size = int((self.n_ctx-300) / k)
+            chunk_size = 32
+            print(f"Chunk size: {chunk_size} \n")
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
             chunk_size=chunk_size, chunk_overlap=chunk_overlap
         )
         split_docs = text_splitter.split_text(text)
-        print(f"Split {len(text)} long text into {len(docs)} documents.\n")
+        print(f"Split {len(text)} long text into {len(split_docs)} documents.\n")
         for doc in split_docs:
             docs.append(
                 Document(
