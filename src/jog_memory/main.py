03fbd3ff8871535a71eb1c4fbd22b4e311adbff5
Bell Eapen <github_public@gulfdoctor.net>
2024-11-26 19:59:30 +0000

    Enhance CLI in jog_memory module with new options for summary expansion and text clearing; update max tokens default value

--- src/jog_memory/main.py
+++ src/jog_memory/main.py
@@ -12,12 +12,14 @@ except ImportError:
 @click.option('--theme', '-t', multiple=False, default='auto',
               help='Theme for summarization (default: auto)')
 @click.option('--n_ctx', '-c', multiple=False, default=2048 + 256, help='Context window size')
-@click.option('--max_tokens', '-m', multiple=False, default=128 + 128, help='Max tokens to generate')
+@click.option('--max_tokens', '-m', multiple=False, default=256, help='Max tokens to generate')
 @click.option('--k', '-k', multiple=False, default=5, help='Number of documents to retrieve')
 @click.option('--n_gpu_layers', '-g', multiple=False, default=-1, help='Number of GPU layers')
-def cli(verbose, file, theme, n_ctx, max_tokens, k, n_gpu_layers):
+@click.option('--expand', '-e', is_flag=True, help="Expand the summary")
+@click.option('--clear', '-x', is_flag=True, help="Clear the text")
+def cli(verbose, file, theme, n_ctx, max_tokens, k, n_gpu_layers, expand, clear):
     if verbose:
-        print("verbose")
+        click.echo("verbose")
     if file: # if file is provided, read the file
         with open(file, 'r') as f:
             text = f.read()
@@ -36,8 +38,24 @@ def cli(verbose, file, theme, n_ctx, max_tokens, k, n_gpu_layers):
     jog_rag = JogRag(
         n_ctx=n_ctx,
     )
+    if clear:
+        jog_memory.clear_text()
+    jog_memory.append_text(text)
     if theme == 'auto':
         theme = jog_memory.find_concept(text)
+        click.echo(f"Theme: {theme}")
+    if expand:
+        expanded = jog_memory.expand_concept(theme)
+        click.echo(f"Expanded concepts: {expanded}")
+    else:
+        expanded = ""
+    # RAG if length of text exceeds context window size
+    if len(jog_memory.get_text()) > (n_ctx-300):
+        docs = jog_rag.split_text(jog_memory.get_text(), "subject", theme, expanded, k=k)
+        context = jog_rag.get_context(theme, expanded, k=k)
+    else:
+        context = jog_memory.get_text()
+    click.secho(f"Summary: {jog_memory.summarize(context, theme, expanded)}\n", fg='green')
 
 def main_routine():
     click.echo("_________________________________________")
