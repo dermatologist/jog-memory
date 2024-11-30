7142cc2a4569a889753db35d85b16f755d446132
Bell Eapen <github_public@gulfdoctor.net>
2024-11-30 00:46:24 +0000

    Refactor JogMemory initialization to support optional model and embedding paths

--- src/jog_memory/jm.py
+++ src/jog_memory/jm.py
@@ -1,57 +1,81 @@
-# python -m pip install llama-cpp-python --prefer-binary --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/basic/cu122
-from huggingface_hub import hf_hub_download, snapshot_download
-from langchain_community.llms import LlamaCpp
-from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
-from langchain_core.prompts import PromptTemplate
-from gensim.models import Word2Vec
-import click
+"""
+ Copyright 2024 Bell Eapen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import logging
 import re
 import tempfile
-import logging
+
+import click
+from gensim.models import Word2Vec
+from huggingface_hub import hf_hub_download, snapshot_download
+# python -m pip install llama-cpp-python --prefer-binary --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/basic/cu122
+from langchain_community.llms import LlamaCpp
+from langchain_core.callbacks import (CallbackManager,
+                                      StreamingStdOutCallbackHandler)
+from langchain_core.prompts import PromptTemplate
 logging.getLogger("langchain_text_splitters.base").setLevel(logging.ERROR)
 from .log import suppress_stdout_stderr
+
+
 class JogMemory:
 
     def __init__(self,
-                 model_name: str = "mradermacher/Llama3-Med42-8B-GGUF",
-                 model_file: str = "Llama3-Med42-8B.Q8_0.gguf",
+                 model_path: str = None, # Use the default model if None
+                 embedding_path: str = None, # Use the default embedding if None
                  n_gpu_layers = -1, # The number of layers to put on the GPU. The rest will be on the CPU. If you don't know how many layers there are, you can use -1 to move all to GPU. 0 for all on CPU.
                  n_batch = 256, # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
                  n_ctx = 2048, # The context window size
                  max_tokens = 128, # The maximum number of tokens to generate
                  temperature = 0.1, # The temperature for sampling
-                 theme_prompt = None,
-                 summary_prompt = None,
-                 expanded_prompt = None,
+                 theme_prompt = None, # The theme prompt
+                 summary_prompt = None, # The summary prompt
+                 expanded_prompt = None, # The expanded prompt
                  ):
         self.tempfile = tempfile.SpooledTemporaryFile(mode='a+t', max_size=10000)
-        self.model_name = model_name
-        self.model_file = model_file
         self.n_gpu_layers = n_gpu_layers
         self.n_batch = n_batch
         self.n_ctx = n_ctx
         self.max_tokens = max_tokens
         self.temperature = temperature
-        self.model_path = hf_hub_download(self.model_name, filename=self.model_file)
+        if model_path is None:
+            self.model_path = hf_hub_download("mradermacher/Llama3-Med42-8B-GGUF", filename="Llama3-Med42-8B.Q8_0.gguf")
+        else:
+            self.model_path = model_path
         # Callbacks support token-wise streaming
         self.callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
         with suppress_stdout_stderr():
             self.llm = LlamaCpp(
                 model_path=self.model_path,  # Download the model file first
-                n_gpu_layers=self.n_gpu_layers,
-                n_batch=self.n_batch,
+                n_gpu_layers=self.n_gpu_layers, # The number of layers to put on the GPU. The rest will be on the CPU. If you don't know how many layers there are, you can use -1 to move all to GPU. 0 for all on CPU.
+                n_batch=self.n_batch, # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
                 n_ctx=self.n_ctx,  # The context window size
                 max_tokens=self.max_tokens,  # The maximum number of tokens to generate
                 temperature=self.temperature,  # The temperature for sampling
-                callback_manager=self.callback_manager,
+                callback_manager=self.callback_manager, # The callback manager
                 verbose=True,  # Verbose is required to pass to the callback manager
             )
             self.theme_prompt = theme_prompt
             self.summary_prompt = summary_prompt
             self.expanded_prompt = expanded_prompt
-            REPO_ID = "garyw/clinical-embeddings-100d-w2v-cr"
-            FILENAME = "w2v_OA_CR_100d.bin"
-            self.word_embedding = Word2Vec.load(snapshot_download(repo_id=REPO_ID)+"/"+FILENAME)
+            if embedding_path is None:
+                REPO_ID = "garyw/clinical-embeddings-100d-w2v-cr"
+                FILENAME = "w2v_OA_CR_100d.bin"
+                self.word_embedding = Word2Vec.load(snapshot_download(repo_id=REPO_ID)+"/"+FILENAME)
+            else:
+                self.word_embedding = Word2Vec.load(embedding_path)
             self.concept = None
 
     def append_text(self, text):
@@ -89,7 +113,6 @@ class JogMemory:
     def trim_after_last_period(self, sentence):
         # Find the last occurrence of a period
         last_period_index = sentence.rfind('.')
-
         # If a period is found, trim the sentence after the last period
         if last_period_index != -1:
             return re.sub(r'\d', '*',sentence[:last_period_index + 1])
